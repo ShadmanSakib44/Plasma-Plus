1c9ddca3c21ac4e928cd594c6d23ed09f67f079c
ShadmanSakib44 <shadmansakib20@iut-dhaka.edu>
2022-10-18 22:42:50 +0600

    Rest of the part

---
+++ plasmaplus/ticker.php
@@ -0,0 +1,62 @@
+
+
+<html>
+<head>
+<style>
+@media screen and (max-width: 80px) {
+  .news {
+    position:relative;margin-left:auto;margin-right:auto;margin-top:400px  ;
+  }
+  .text1{
+  	box-shadow:none !important;
+    position:relative;
+      margin-left:auto;margin-right:auto;
+  }
+}
+.blue {
+    background: #FFD1D1;
+}
+
+.news {
+    box-shadow: inset 0 -15px 30px rgba(10,4,60,0.4), 0 5px 10px rgba(10,20,100,0.5);
+       width: 100%;
+       height:50px;
+    margin-top:0px;
+    overflow: hidden;
+
+    border-radius: 4px;
+    padding: 1px;
+    -webkit-user-select: none;
+
+}
+
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800&display=swap");
+
+.news span {
+    float: left;
+    color: #000000;
+    padding: 9px;
+    position: relative;
+    top: 1%;
+    box-shadow: inset 0 -15px 30px rgba(0,0,0,0.4);
+    font: 14px "Poppins", sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -webkit-user-select: none;
+    cursor: pointer;
+}
+
+.text1{
+
+	box-shadow:none !important;
+  position:relative;
+    width:90%
+}
+</style>
+</head>
+<body>
+<div class="news blue">
+<span style="background-color:#E26868;width:125px;height:50px">Latest Updates</span><span class="text1" ><marquee>First blood donation camp to be organised in Boardbazar,Gazipur By IUT in collabration with Plasma Plus <b>19/10/2022 at the Auditorium of IUT</b> Come and Be a Part of this Noble Cause</marquee></span>
+</div>
+</body>
+</head>
+</html>
